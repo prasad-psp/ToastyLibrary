2a5ef36137f4a612e1d137310d91acaae6891059
Admin <prasad.parshram123@gmai.com>
2021-10-30 17:18:32 +0530

    added new feature long toast

--- androidtoastylibrary/src/main/java/com/psp/androidtoastylibrary/SimpleToast.java
+++ androidtoastylibrary/src/main/java/com/psp/androidtoastylibrary/SimpleToast.java
@@ -9,4 +9,8 @@ public class SimpleToast {
     public static void makeShortToast(Context context,String msg) {
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
+
+    public static void makeLongToast(Context context,String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+    }
 }
